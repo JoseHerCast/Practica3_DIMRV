c743bf814eb39bd04f77275bf92f8c739ceb39bc
José Hernández <Jose_Hernandez_c@outlook.com>
2021-11-04 23:15:33 -0600

    Práctica 3 - Carga de modelos obj - DIMRV

--- model.cpp
+++ model.cpp
@@ -9,32 +9,39 @@ bool obj3dmodel::readfile(const char* filename)
         printf("Impossible to open the file !\n");
         return false;
     }
-
+    
     char linea[55];
     char *aux;
+    //Leemos linea a linea
     while (fgets(linea, sizeof(linea), file)) {
         switch (linea[0])
         {
         case 'V':
         case 'v':
+            //Si nuestra linea es Vt (coordenada de textura)
             if (linea[1]=='t') {
                 break;
             }
+            //Si nuestra linea es Vn (Normal)
             else if (linea[1]=='n') {
                 break;
             }
+            //Si nuestra linea es un simple vertice
             else {
                 vertex v;
+                //strtok(); le el buffer de entrada del fichero hasta que el caracter concuerda con el proporcionado
                 aux = strtok(linea, " "); //Descartamos el primer espacio que es en donde comienzan los valores númericos
                 //Obtenemos los valores posteriores separados por un espacio
                 aux = strtok(NULL, " "); v.x = atof(aux);
                 aux = strtok(NULL, " "); v.y = atof(aux);
                 aux = strtok(NULL, " "); v.z = atof(aux);
+                //Añadimos el valor leido a nuestro arreglo de vertices
                 this->vertexes.push_back(v);
             }
             break;
         case 'F':
         case 'f':
+            //Si nuestra linea es una cara (Face)
             face f;
             aux = strtok(linea, " "); //Descartamos el primer espacio que es en donde comienzan los valores númericos
             //Obtenemos los valores posteriores separados por un espacio
@@ -43,7 +50,8 @@ bool obj3dmodel::readfile(const char* filename)
             aux = strtok(NULL, "/"); f.v2 = atoi(aux);
             aux = strtok(NULL, " ");
             aux = strtok(NULL, "/"); f.v3 = atoi(aux);
-
+            //Añadimos el valor leido a nuestro arreglo de caras (de momento solo los valores de los vertices)
+            //Se estan omitiendo las normales y el texturizado
             this->faces.push_back(f);
 
             break;
@@ -58,8 +66,10 @@ bool obj3dmodel::readfile(const char* filename)
 void obj3dmodel::draw()
 {
     glBegin(GL_TRIANGLES);
+    //Dibujamos las caras
     for (int i = 0; i < faces.size(); i++)
     {
+        //Almacenamos el valor de los vertices recorriendo cara por cara
         vertex v1 = vertexes[faces[i].v1 - 1];
         vertex v2 = vertexes[faces[i].v2 - 1];
         vertex v3 = vertexes[faces[i].v3 - 1];
--- model.h
+++ model.h
@@ -4,18 +4,23 @@
 
 class obj3dmodel
 {
+    //Estos serán nuestros vertices
     struct vertex {
         double x;
         double y;
         double z;
     };
+    //Cada cara estará formada por 3 vertices (triangulos)
     struct face {
         unsigned int v1, v2, v3;
     };
+    //Declaramos los arreglos de vertices y de caras
     std::vector<vertex> vertexes;
     std::vector<face> faces;
 
 public:
+    //Función de lectura del archivo
     bool readfile(const char* filename);
+    //Función de dibujo del modelo
     void draw();
 };
